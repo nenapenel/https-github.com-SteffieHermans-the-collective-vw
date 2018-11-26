29cb4124d5b2eaca78532a41ae617d0327bde9f2
Nena Penel <nena.penel@student.howest.be>
2018-11-26 15:50:20 +0100

    Initial Commit

---
+++ testingFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testingFirebase
+//
+//  Created by Nena Penel on 26/11/2018.
+//  Copyright © 2018 Nena Penel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
