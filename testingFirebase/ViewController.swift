57abaf0bc5686043c3634be5da523bdecf58533f
Nena Penel <nena.penel@student.howest.be>
2018-11-27 09:48:59 +0100

    Testing to commit to the master branch

--- testingFirebase/ViewController.swift
+++ testingFirebase/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var refNames: DatabaseReference!
 
+    @IBOutlet weak var voorNaam: UITextField!
+    @IBOutlet weak var achterNaam: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    @IBAction func voegToe(_ sender: UIButton) {
+        addNames()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refNames = Database.database().reference().child("names");
+    }
+    
+    func addNames(){
+        let key = refNames.childByAutoId().key
+        let name = ["id": key,
+                     "voorNaam": voorNaam.text! as String,
+                     "achternaam": achterNaam.text! as String
+                    ]
+        refNames.child(key).setValue(name)
+        labelMessage.text = "Je bent toegevoegd aan de lijst"
     }
 
 
